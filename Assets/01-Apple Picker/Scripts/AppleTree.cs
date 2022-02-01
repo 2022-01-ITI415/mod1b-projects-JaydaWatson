c2418774722b052e7695f0a382a32f3517dee962
JaydaWatson <60038607+JaydaWatson@users.noreply.github.com>
2022-02-01 14:10:52 -0500

    Setting Physics Layers

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -16,16 +16,21 @@ public class AppleTree : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke ("DropApple", 2f);
+    }
+
+    void DropApple()
+    {
+        GameObject apple = Instantiate < GameObject;
+        apple.transform.position = transform.position;
+        Invoke ("DropApple", secondsBetweenAppleDrop);
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;      
- 
         pos.x += speed * Time.deltaTime;       
- 
         transform.position = pos;    
 
         if ( pos.x < -leftAndRightEdge ) 
