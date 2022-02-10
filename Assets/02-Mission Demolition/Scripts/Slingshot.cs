ec586838d7065db0dabcd416ec8ebc6dc4050ef9
JaydaWatson <60038607+JaydaWatson@users.noreply.github.com>
2022-02-10 18:37:21 -0500

    Instantiating a projectile

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+
+    public GameObject prefabProjectile;
+    
+    [Header("Set Dynamically")]
+
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+
     void OnMouseEnter()
     {
         print("Slingshot:OnMouseEnter()");
@@ -13,13 +32,21 @@ public class Slingshot : MonoBehaviour
     {
         print("Slingshot:OnMouseExit()");
     }
-    // Start is called before the first frame update
+    
+    void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
