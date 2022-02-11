da2b3074c3129de84ebfdb5d1d79e64ecf54ff48
JaydaWatson <60038607+JaydaWatson@users.noreply.github.com>
2022-02-10 19:44:58 -0500

    Setting limits to Follow Cam

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -7,6 +7,11 @@ public class FollowCam : MonoBehaviour
 
     static public GameObject POI;
 
+    [Header("Set in Inspector")]
+
+    public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
+
     [Header("Set Dynamically")]
 
     public float camZ;
@@ -26,6 +31,11 @@ public class FollowCam : MonoBehaviour
         if (POI == null) return;
 
         Vector3 destination = POI.transform.position;
+
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
+        destination = Vector3.Lerp(transform.position, destination, easing);
 
         destination.z = camZ;
 
